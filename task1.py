ff02ddb18c1e69b14e59910dc19562d433dd78a3
debadrita17 <debadritadas2002@gmail.com>
2025-07-18 21:03:58 +0530

    upload task1 and task2 for assignment 1

---
+++ task1.py
@@ -0,0 +1,12 @@
+#TASK1
+#performs the basic mathematical operations(add,subtract,multiply,divide) on two numbers as input.
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+c= a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition: ",c)
+print("subtraction: ",d)
+print("multiplication: ",e)
+print("division: ",f)
---
+++ task2.py
@@ -0,0 +1,6 @@
+#TASK2
+#print a personalized greeting message using a user's first name & last name as input and concatenates into full name
+firstname=input("enter first name: ")
+lastname=input("enter last name: ")
+fullname=firstname+" "+lastname
+print(fullname)
